ea42ba1eeca605944c1b8aa10407ae91e7525529
NA <aseshgiri@yahoo.com>
2017-03-27 16:05:52 -0500

    first commit

---
+++ Matplotlib.py
@@ -0,0 +1,16 @@
+from matplotlib import pyplot as plt
+from matplotlib import style
+
+x = [5,6,7,8]
+y = [7,3,8,3]
+
+print(len(x))
+print(len(y))
+
+plt.plot(x,y)
+
+plt.title('Epic Char')
+plt.ylabel('Y axis')
+plt.xlabel('X asix')
+
+plt.show
